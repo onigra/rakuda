482a14591a4c45081d786aebb6264cbdcdb413c9
onigra <3280467rec@gmail.com>
2026-07-25 23:45:31 +0900

    add url generator test

---
+++ test/url_generator_test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "rakuda/url_generator"
+
+class TestUrlGenerator < Test::Unit::TestCase
+  def setup
+    @gen = Rakuda::UrlGenerator.new("/blog/:year/:month/:day/:slug/")
+  end
+
+  def test_post_url
+    date = Time.new(2026, 7, 20, 0, 0, 0, "+09:00")
+    url = @gen.post_url(date, "hello-world")
+    assert_equal "/blog/2026/07/20/hello-world/", url
+  end
+
+  def test_category_slug
+    assert_equal "gh-actions", Rakuda::UrlGenerator.category_slug("gh-actions")
+    assert_equal "mysql", Rakuda::UrlGenerator.category_slug("MySQL")
+  end
+
+  def test_category_url
+    assert_equal "/categories/ruby/", @gen.category_url("ruby")
+  end
+
+  def test_home_pagination_urls
+    assert_equal "/", @gen.home_page_url(1)
+    assert_equal "/page/2/", @gen.home_page_url(2)
+  end
+end
