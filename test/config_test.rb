b726445bce7dd8a06471c04d692d6831be9807f6
onigra <3280467rec@gmail.com>
2026-07-25 22:21:22 +0900

    add config test

---
+++ test/config_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+require "rakuda/config"
+
+class TestConfig < Test::Unit::TestCase
+  def setup
+    @path = File.expand_path("fixtures/minimal/site.yml", __dir__)
+  end
+
+  data(
+    "base_url" => ["https://example.com", :base_url],
+    "paginate" => [10, :paginate],
+    "permalink_post" => ["/blog/:year/:month/:day/:slug/", :permalink_post]
+  )
+  def test_loads_site_yml(data)
+    expected, attribute = data
+    config = Rakuda::Config.load(@path)
+    assert_equal expected, config.public_send(attribute)
+  end
+end
