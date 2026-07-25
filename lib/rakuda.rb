103e0dcf9d0390c7dccfdd190305390af3b579cb
onigra <3280467rec@gmail.com>
2026-07-26 01:17:14 +0900

    require

--- lib/rakuda.rb
+++ lib/rakuda.rb
@@ -3,6 +3,9 @@
 require_relative "rakuda/version"
 require_relative "rakuda/config"
 require_relative "rakuda/url_generator"
+require_relative "rakuda/models/post"
+require_relative "rakuda/models/page"
+require_relative "rakuda/content_loader"
 
 module Rakuda
   class Error < StandardError; end
