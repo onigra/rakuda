f358183fb79db55f245fbcde54d5dbeb9f540e8d
onigra <3280467rec@gmail.com>
2026-07-25 22:21:39 +0900

    add config loader

---
+++ lib/rakuda/config.rb
@@ -0,0 +1,25 @@
+require "yaml"
+
+module Rakuda
+  Config = Data.define(
+    :base_url, :title, :language, :author, :paginate,
+    :permalink_post, :taxonomy_category, :params
+  )
+
+  class Config
+    def self.load(path)
+      data = YAML.safe_load_file(path, permitted_classes: [Date, Time], aliases: true)
+
+      new(
+        base_url: data.fetch("base_url"),
+        title: data.fetch("title"),
+        language: data.fetch("language", "ja"),
+        author: data.fetch("author", ""),
+        paginate: data.fetch("paginate", 10),
+        permalink_post: data.dig("permalinks", "post"),
+        taxonomy_category: data.dig("taxonomies", "category") || "categories",
+        params: data.fetch("params", {})
+      )
+    end
+  end
+end
