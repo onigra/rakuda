33c0d1e809369a41a07579f9dbc41803f1a58df0
onigra <3280467rec@gmail.com>
2026-07-26 02:48:21 +0900

    余計な書き方してることに気づいた

--- lib/rakuda/content_loader.rb
+++ lib/rakuda/content_loader.rb
@@ -7,18 +7,9 @@ require_relative "page_loader"
 module Rakuda
   class ContentLoader
     def self.load(source_dir, config)
-      new(source_dir, config).load
-    end
-
-    def initialize(source_dir, config)
-      @source_dir = source_dir
-      @config = config
-    end
-
-    def load
       Models::Contents.new(
-        posts: PostLoader.load(@source_dir, @config),
-        pages: PageLoader.load(@source_dir)
+        posts: PostLoader.load(source_dir, config),
+        pages: PageLoader.load(source_dir)
       )
     end
   end
