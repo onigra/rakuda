e35d050313a79a409cecce294d28563851b23eb2
onigra <3280467rec@gmail.com>
2026-07-26 18:52:45 +0900

    Pipeline が参照する固定ページ（/about/ 等） を出力するための generatorを 実装

---
+++ lib/rakuda/generators/page.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require_relative "../site_context"
+
+module Rakuda
+  module Generators
+    ###
+    #
+    # Pipeline が参照する固定ページ（/about/ 等） を出力するための generator
+    #
+    class Page
+      #: @rbs config: Config
+      #: @rbs page: Page
+      #: @rbs renderer: TemplateRenderer
+      #: @rbs markdown: MarkdownRenderer
+      #: @rbs return: Hash[Symbol, String]
+      def initialize(config:, page:, renderer:, markdown:)
+        @config = config
+        @page = page
+        @renderer = renderer
+        @markdown = markdown
+        @site = SiteContext.build(config)
+      end
+
+      #: @rbs return: Hash[Symbol, String]
+      def generate
+        content_html = @markdown.render(@page.body)
+        html = @renderer.render("single", {
+          site: @site,
+          page: page_to_hash(content_html)
+        })
+        {url: @page.url, content: html}
+      end
+
+      private
+
+      #: @rbs content_html: String
+      #: @rbs return: Hash[Symbol, untyped]
+      def page_to_hash(content_html)
+        {
+          title: @page.title,
+          date: @page.date,
+          url: @page.url,
+          categories: @page.categories,
+          content: content_html,
+          summary: @markdown.render(@page.summary),
+          is_home: false
+        }
+      end
+    end
+  end
+end
