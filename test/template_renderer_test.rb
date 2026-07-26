fdae932c6bbced04cc912158b8826356cb1aad2e
onigra <3280467rec@gmail.com>
2026-07-26 14:41:58 +0900

    add template renderer test

---
+++ test/template_renderer_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "rakuda/template_renderer"
+require "rakuda/site_context"
+require "rakuda/config"
+
+class TestTemplateRenderer < Test::Unit::TestCase
+  def test_renders_erb_layout
+    # Given
+    root = File.expand_path("fixtures/minimal", __dir__)
+    config = Rakuda::Config.load(File.join(root, "site.yml"))
+    renderer = Rakuda::TemplateRenderer.new(File.join(root, "layouts"))
+    
+    # When
+    html = renderer.render("single", {
+      site: Rakuda::SiteContext.build(config),
+      page: { title: "Hi", content: "<p>Body</p>" },
+    })
+    
+    # Then
+    assert_include(html, "<h1>Hi</h1>")
+    assert_include(html, "<p>Body</p>")
+  end
+end
