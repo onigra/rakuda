ad47cc89f97b03524ffa70353721448d2c7ff011
onigra <3280467rec@gmail.com>
2026-07-26 18:56:09 +0900

    add Generator::Page test

---
+++ test/generators/page_test.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "rakuda/generators/page"
+require "rakuda/page_loader"
+
+class TestPageGenerator < Test::Unit::TestCase
+  def setup
+    @root = File.expand_path("../fixtures/minimal", __dir__)
+    @config = Rakuda::Config.load(File.join(@root, "site.yml"))
+    @renderer = Rakuda::TemplateRenderer.new(File.join(@root, "layouts"))
+    @markdown = Rakuda::MarkdownRenderer
+  end
+
+  def test_renders_static_page
+    # Given
+    page = Rakuda::PageLoader.load(@root).first
+
+    # When
+    result = Rakuda::Generators::Page.new(
+      config: @config,
+      page: page,
+      renderer: @renderer,
+      markdown: @markdown
+    ).generate
+
+    # Then
+    assert_equal "/about/", result[:url]
+    assert_include(result[:content], "<h1>About</h1>")
+    assert_include(result[:content], "About body.")
+  end
+end
