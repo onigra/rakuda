21d1336f1fd2714fd2f1203688556a6412c672a1
onigra <3280467rec@gmail.com>
2026-07-26 15:11:51 +0900

    fix style

--- test/template_renderer_test.rb
+++ test/template_renderer_test.rb
@@ -11,13 +11,13 @@ class TestTemplateRenderer < Test::Unit::TestCase
     root = File.expand_path("fixtures/minimal", __dir__)
     config = Rakuda::Config.load(File.join(root, "site.yml"))
     renderer = Rakuda::TemplateRenderer.new(File.join(root, "layouts"))
-    
+
     # When
     html = renderer.render("single", {
       site: Rakuda::SiteContext.build(config),
-      page: { title: "Hi", content: "<p>Body</p>" },
+      page: {title: "Hi", content: "<p>Body</p>"}
     })
-    
+
     # Then
     assert_include(html, "<h1>Hi</h1>")
     assert_include(html, "<p>Body</p>")
