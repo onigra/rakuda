8f3aa794352af71fdb0e4dce9e37158671143840
onigra <3280467rec@gmail.com>
2026-07-26 16:34:23 +0900

    add require

--- lib/rakuda.rb
+++ lib/rakuda.rb
@@ -6,6 +6,12 @@ require_relative "rakuda/url_generator"
 require_relative "rakuda/models/post"
 require_relative "rakuda/models/page"
 require_relative "rakuda/content_loader"
+require_relative "rakuda/generators/post"
+require_relative "rakuda/generators/home"
+require_relative "rakuda/generators/section"
+require_relative "rakuda/generators/taxonomy"
+require_relative "rakuda/generators/rss"
+require_relative "rakuda/generators/robots"
 
 module Rakuda
   class Error < StandardError; end
