ad41aa712dee8041ba04adebdc3e93ad9488b240
onigra <3280467rec@gmail.com>
2026-08-02 15:27:42 +0900

    markdownを2回レンダリングしないようにキャッシュする

--- lib/rakuda/markdown_renderer.rb
+++ lib/rakuda/markdown_renderer.rb
@@ -8,11 +8,18 @@ module Rakuda
   module MarkdownRenderer
     SKIP_TAGS = %w[a pre code].freeze
 
+    @html_cache = {} #: Hash[String, String]
+
     def self.render(markdown, autolink: true)
-      html = Kramdown::Document.new(markdown, input: "GFM").to_html
+      html = html_for(markdown)
       return html unless autolink && markdown.include?("http")
 
       Rinku.auto_link(html, :all, nil, SKIP_TAGS)
     end
+
+    def self.html_for(markdown)
+      @html_cache[markdown] ||= Kramdown::Document.new(markdown, input: "GFM").to_html
+    end
+    private_class_method :html_for
   end
 end
